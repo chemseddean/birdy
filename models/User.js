e86a8258d44830151d2d5ac2d3eb43b8cfb3578c
chemseddean <bensafiachemseddine@gmail.com>
2021-02-23 23:55:49 +0100

    User registration schema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+    id : {
+        type: String,
+        required: true
+    }, 
+    firstName : {
+        type: String,
+        required: true
+    }, 
+    lastName : {
+        type: String,
+        required: true
+    }, 
+    email: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    }, 
+    avatar: {
+        type: String
+    }
+})
+
+module.exports = User = mongoose.model('user', UserSchema)
