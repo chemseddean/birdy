4ebc888e159354c3cb3a5850ab67e791e270e706
chemseddean <bensafiachemseddine@gmail.com>
2021-02-25 21:52:51 +0100

    Register user and inserting in DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,11 @@ const bcrypt = require('bcrypt')
 const Joi = require('joi')
 const User = require('../../models/User')
 const router = express.Router();
-const create_db = require('../../config/usersDB')
+const db = require('../../config/usersDB')
+
 
 const joiUserSchema = Joi.object({
-    id: Joi.number().required(),
+    username: Joi.string().required(),
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     email: Joi.string().email(),
@@ -16,43 +17,61 @@ const joiUserSchema = Joi.object({
 // @route POST /api/users
 // @route Register user
 // @route Public
-
 router.post('/', async (req, res) => {
     // validation schema 
-    const result = await joiUserSchema.validateAsync(req.body)
+    try{
+        const result = await joiUserSchema.validateAsync(req.body)
     if (result.error) {
         res.status(400).send(result.error.details[0].message)
         return
     }
-    var content = req.body
-    const hashedpassword = await bcrypt.hash(req.body.password, 10)
-    content.password = hashedpassword
-    const newUser = new User(content)
-    try {
-        
-        create_db()
-        
-        try {
-            await db.run(`INSERT INTO users VALUES(?,?,?,?,?)`, 
-                [content.id, content.firstName, content.lastName, content.email, content.password], 
-                function(err) {
-                    if (err) {
-                      return console.log(err.message);
-                    }
-                    // get the last insert id
-                    console.log(`A row has been inserted with rowid ${this.lastID}`);
-                  });
-        } catch (error) {
-            console.error(error.message)
-        }
-        await newUser.save()
-        res.status(201)
-
-    } catch (error) {
-        console.log('Server error', error)
+    }catch(error){
+        res.json({error: error.message})
     }
-    ////// add user to data base
-    //res.status(201).send(newUser)
+
+    const user = {
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+    } = req.body
+
+    // var errors = []
+    // if (!user.username)
+    //     errors.push("No username specified")
+    // if (!user.firstName)
+    //     errors.push("No first name specified")
+    // if (!user.lastName)
+    //     errors.push("No last name specified")
+    // if (!user.email)
+    //     errors.push("No email specified")
+    // if (!user.password)
+    //     errors.push("No password specified")
+
+    const hashedpassword = await bcrypt.hash(req.body.password, 10)
+    user.password = hashedpassword
+    const newUser = new User(user)
+
+    try {
+    var insert = 'INSERT INTO user (username, firstName, lastName, email, password) VALUES (?,?,?,?,?)'
+    var params = [user.username, user.firstName, user.lastName, user.email, user.password]
+        db.run(insert, params, (error)=>{
+            if(error){
+                //res.status(400).json({"Error": error.message})
+                console.error(error.message)
+                return
+            }
+        })
+        res.json({
+            "message": "User created",
+            "user": user,
+            "user_id": this.lastID
+        })
+        newUser.save()
+    } catch (error) {
+        res.send("Server error")
+    }  
 });
 
 
