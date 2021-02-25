5f981b253eb4314ec3dc15093723258f82309ab2
chemseddean <bensafiachemseddine@gmail.com>
2021-02-25 21:50:31 +0100

    id changed to username

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    id : {
-        type: Number,
+    username : {
+        type: String,
         required: true
     }, 
     firstName : {
