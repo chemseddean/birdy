0a7f8e9dd54f9e905d7337fb4f71ea874c3b681a
chemseddean <bensafiachemseddine@gmail.com>
2021-02-25 03:31:12 +0100

    SQLite3 install

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 const Joi = require('joi')
 const User = require('../../models/User')
 const router = express.Router();
-
+const create_db = require('../../config/usersDB')
 
 const joiUserSchema = Joi.object({
     id: Joi.number().required(),
@@ -12,6 +12,10 @@ const joiUserSchema = Joi.object({
     email: Joi.string().email(),
     password: Joi.string().min(8)
 })
+
+// @route POST /api/users
+// @route Register user
+// @route Public
 
 router.post('/', async (req, res) => {
     // validation schema 
@@ -25,16 +29,37 @@ router.post('/', async (req, res) => {
     content.password = hashedpassword
     const newUser = new User(content)
     try {
+        
+        create_db()
+        
+        try {
+            await db.run(`INSERT INTO users VALUES(?,?,?,?,?)`, 
+                [content.id, content.firstName, content.lastName, content.email, content.password], 
+                function(err) {
+                    if (err) {
+                      return console.log(err.message);
+                    }
+                    // get the last insert id
+                    console.log(`A row has been inserted with rowid ${this.lastID}`);
+                  });
+        } catch (error) {
+            console.error(error.message)
+        }
         await newUser.save()
+        res.status(201)
+
     } catch (error) {
         console.log('Server error', error)
     }
     ////// add user to data base
-    res.status(201).send(newUser)
+    //res.status(201).send(newUser)
 });
 
 
-router.get('/', (req, res) => {
+// @route GET /api/users/:user_id
+// @route GET user by id
+// @route Public
+router.get('/:user_id', (req, res) => {
     res.send('okay ca marche')
 })
 module.exports = router;
