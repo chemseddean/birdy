d81ca7e209611b45b1295e75391dcf4d545ecc92
chemseddean <bensafiachemseddine@gmail.com>
2021-02-25 21:52:04 +0100

    UsersDB config file

--- config/usersDB.js
+++ config/usersDB.js
@@ -1,27 +1,27 @@
 var sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./DB/Birdy.db', (error) => {
+const db = new sqlite3.Database('./DB/birdy.db', (error) => {
     if (error) {
-      return console.error(error.message);
+      console.error(error.message);
+      throw error
+    }else{
+      console.log('Successfuly connected to Birdy db.');
+      let sql = `CREATE TABLE IF NOT EXISTS users(
+        id VARCHAR(255) PRIMARY KEY,
+        firstName VARCHAR(255) NOT NULL,
+        lastName VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL)`
+
+      db.run(sql, (error) => {
+        if (error){
+          //Table already created
+          console.error(error.message)
+        }else{
+          console.log("Birdy db successfuly created")
+        }
+      })
     }
-    console.log('Successfuly connected to Birdy db.');
   });
 
-let sql = `CREATE TABLE IF NOT EXISTS users(
-            id VARCHAR(255) PRIMARY KEY,
-            firstName VARCHAR(255) NOT NULL,
-            lastName VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL)`
-
-const create_db = async () => {
-        
-        await db.run(sql, function(error) {
-            if (error)
-                return console.log(error.message)
-            else
-                console.log("success")
-        })
-    }
-
-module.exports = create_db
+module.exports = db
