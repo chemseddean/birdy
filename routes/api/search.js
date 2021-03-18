d6666d8ee8559adddf48c51a814a2c7a6df42d71
chemseddean <bensafiachemseddine@gmail.com>
2021-03-18 22:48:02 +0100

    SearchRoute

---
+++ routes/api/search.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User')
+
+// @route   POST api/search/users
+// @desc    Search users
+// @access  Public 
+router.post('/users', async (req, res) => {
+    const userPattern = RegExp("^"+req.body.username)
+    try {
+        const users = await User.find({username: userPattern})
+    if (users){
+        return res.status(200).json(users)
+    }
+    else{
+        return res.
+            status(400).
+            json({
+                 errors : [{ msg : 'User does not exist'}]
+                });
+    }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+
+module.exports = router
