6296340c7e82b0eca28627a4817ba371071700da
chemseddean <bensafiachemseddine@gmail.com>
2021-04-14 20:22:44 +0200

    Client side

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, {Fragment} from 'react'
+import Navbar from './components/layouts/Navbar'
+import Landing from './components/layouts/Landing'
+
+const App = () => 
+      <Fragment>
+        <Navbar />
+        <Landing />
+      </Fragment> 
+
+export default App;
