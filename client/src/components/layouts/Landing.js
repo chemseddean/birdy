60203782b4570a68e43420c856b025fb2885111c
chemseddean <bensafiachemseddine@gmail.com>
2021-04-19 01:13:32 +0200

    a tags replaced with Link

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
     return (
         <section className="landing">
-        <div className="dark-overlay">
-          <div className="landing-inner">
-            <h1 className="x-large">Birdy</h1>
-            <p className="lead">
-              Twitter clone
-            </p>
-            <div className="buttons">
-              <a href="register.html" className="btn btn-primary">Sign Up</a>
-              <a href="login.html" className="btn btn-light">Login</a>
-            </div>
-          </div>
+         <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Birdy</h1>
+          <p className="lead">
+            Twitter clone
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">Register</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
+          </div> 
         </div>
+      </div>
       </section>
     )
 }
