1fae595f5e4155e97dab7590bdd2cb03c6726d89
chemseddean <bensafiachemseddine@gmail.com>
2021-04-19 01:13:58 +0200

    a tags replaced with Link

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
         <h1>
-          <a href="index.html">Birdy</a>
+          <Link to="/"> Birdy </Link>
         </h1>
         <ul>
-          <li><a href="profiles.html">About</a></li>
-          <li><a href="register.html">Register</a></li>
-          <li><a href="login.html">Login</a></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
         </ul>
       </nav>
     )
