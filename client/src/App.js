055d337df24ac27e5f7fa9c0db81d70a482e2742
chemseddean <bensafiachemseddine@gmail.com>
2021-04-19 01:12:38 +0200

    RouteConfig for register & login

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import React, {Fragment} from 'react'
 import Navbar from './components/layouts/Navbar'
 import Landing from './components/layouts/Landing'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 const App = () => 
-      <Fragment>
+      <Router>
+        <Fragment>
         <Navbar />
-        <Landing />
-      </Fragment> 
+        <Route exact path="/" component= {Landing}/>
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+          </Switch>
+        </section>
+      </Fragment>
+      </Router> 
 
 export default App;
