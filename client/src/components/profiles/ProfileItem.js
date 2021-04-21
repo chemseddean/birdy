1c9a70596e1c6219733a580fc65ff44c8ad1c9e6
chemseddean <bensafiachemseddine@gmail.com>
2021-04-21 13:58:41 +0200

    Profile item

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+const ProfileItem = ({ profile: {
+    githubusername,
+    status, 
+    location, 
+    intrests
+}}) => {
+    return (
+        <div className="profile bg-light">
+            <i class="far fa-user fa-7x"></i>
+            <div>
+                <h2>{githubusername}</h2>
+                <p>{status}</p>
+                <p className="my-1"><i class="fas fa-thumbtack"></i>{location && <span>{' '}{location}</span>}</p>
+                <Link to={`/profils/user/${githubusername}`} className="btn btn-primary">
+                    View Profile
+                </Link>
+            </div>
+            <ul>
+                {intrests.slice(0, 5).map((intrest, index)=>(
+                    <li key={index} className="text-primary">
+                        <i className="fas fa-check"></i> {intrest}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+ProfileItem.propTypes = {
+    profile: PropTypes.object.isRequired,
+}
+
+export default ProfileItem
