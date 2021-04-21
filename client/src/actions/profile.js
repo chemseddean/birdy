d84c456a32620ba4ce42185c30cd679fee951857
chemseddean <bensafiachemseddine@gmail.com>
2021-04-21 03:07:25 +0200

    Get current/byId/all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,25 +3,63 @@ import { setAlert } from './alert'
 import{
     GET_PROFILE, 
     PROFILE_ERROR,
+    CLEAR_PROFILE,
+    GET_PROFILES
+
 } from './types'
 
 // Get current user profile
-// export const getCurrentProfile = () => 
-// async dispatch => {
-//     try {
-//         const res = await axios.get('/api/profils/me')
-//         dispatch({
-//             type: GET_PROFILE, 
-//             payload: res.data,
-//         })
-//     } catch (error) {
-//         dispatch({
-//             type: PROFILE_ERROR, 
-//             payload: { msg: error.response.statusText, status: error.response.status}
-//         })
-//     }
-// }
+export const getCurrentProfile = () => 
+async dispatch => {
+    try {
+        const res = await axios.get('/api/profils/me')
+        dispatch({
+            type: GET_PROFILE, 
+            payload: res.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR, 
+            //payload: { msg: error.response.statusText, status: error.response.status}
+        })
+    }
+}
 
+//Get all profiles
+export const getProfiles = () => 
+async dispatch => {
+    dispatch({ type: CLEAR_PROFILE})
+    try {
+        const res = await axios.get('/api/profils')
+        dispatch({
+            type: GET_PROFILES, 
+            payload: res.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR, 
+            //payload: { msg: error.response.statusText, status: error.response.status}
+        })
+    }
+}
+
+//Get profile by id
+export const getProfileById = userId => 
+async dispatch => {
+    
+    try {
+        const res = await axios.get(`/api/profils/user/${userId}`)
+        dispatch({
+            type: GET_PROFILE, 
+            payload: res.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR, 
+            //payload: { msg: error.response.statusText, status: error.response.status}
+        })
+    }
+}
 // Create/Update profile
 export const createProfile = (formData, history, edit = false) => async dispatch => {
     try {
