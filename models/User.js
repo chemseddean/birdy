ce485be5dde909063999be8fd1709eeeb6ffbbbe
chemseddean <bensafiachemseddine@gmail.com>
2021-04-21 13:59:23 +0200

    Trying to add avatar to user schema

--- models/User.js
+++ models/User.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username : {
         type: String
+    }, 
+    avatar : {
+        type: String
     }
 }, {timestamps: true})
 
